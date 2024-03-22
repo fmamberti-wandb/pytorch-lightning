490295cdf7e14325c1a7e7ffab993735b2312d87
Francesco Mamberti <francescomamberti@Francesco-Mamberti-NMWCQGGFDV.local>
2024-03-22 10:12:27 +0000

    added print statement to see if function to log checkpoints are run

--- src/lightning/pytorch/callbacks/model_checkpoint.py
+++ src/lightning/pytorch/callbacks/model_checkpoint.py
@@ -391,8 +391,11 @@ class ModelCheckpoint(Checkpoint):
         self._last_checkpoint_saved = filepath
 
         # notify loggers
+        print(f"trainer.is_global_zero: {trainer.is_global_zero}")
         if trainer.is_global_zero:
+            print("looping through loggers")
             for logger in trainer.loggers:
+                print(f"running after_save_checkpoint for logger: {logger}")
                 logger.after_save_checkpoint(proxy(self))
 
     @staticmethod
